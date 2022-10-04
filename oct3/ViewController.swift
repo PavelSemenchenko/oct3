533b88263683bccb13f220cf9c33b4509533958b
Pavel Semenchenko <imagemakerspa@icloud.com>
2022-10-04 17:46:27 +0300

    вычисляемые переменные

--- oct3/ViewController.swift
+++ oct3/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let student = Student()
+        student.name = "John Doe"
+        student.direction = "Programming"
+        student.city = "London"
+        student.street = "Tower"
+        student.house = 22
+        
+        label1.text = student.name
+        label2.text = student.adress
+        
+                
     }
-
-
+    
+    class Student {
+        var name: String = ""
+        var direction: String = ""
+        var city: String = ""
+        var street: String = ""
+        var house: Int = 0
+        
+        var adress: String {
+            get {
+                print("adress computing ...")
+                return "\(city) st.\(street), \(house)"
+            }
+        }
+    }
+    
 }
 
